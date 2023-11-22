2fa6f6ad48c8009dd49fb6f668219ac9690fa1e8
BautiMiano <114349383+BautiMiano@users.noreply.github.com>
2023-11-22 20:02:11 -0300

    Add files via upload

--- js/formulario.js
+++ js/formulario.js
@@ -1,11 +1,22 @@
-var inputs = document.getElementsByClassName("formulario__input");
-for (var i = 0; i < inputs.length; i++){
-    inputs[i].addEventListener("keyup",function(){
-        if(this.value.length>=1){
-            this.nextElementSibling.classList.add("fijar");
-        }else{
-            this.nextElementSibling.classList.remove("fijar");
-        }
-    });
+function validarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var correo = document.getElementById('correo').value;
+    var mensaje = document.getElementById('mensaje').value;
+    
 
+    if (nombre === '' || apellido === '' || correo === '' || mensaje === '') {
+        alert('Por favor, complete todos los campos.');
+        return false; // 
+    }
+
+    var emailCorrecto = /^\S+@\S+\.\S+$/;
+    if (!emailCorrecto.test(correo)) {
+        alert('Por favor, ingrese un correo electrónico válido.');
+        return false; // 
+    }
+
+    return true; // 
+    
+    
 }
